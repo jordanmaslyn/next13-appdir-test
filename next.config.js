72265a9c666f90158542723467a842795bffe4f2
Jordan Maslyn <jordan.maslyn@wpengine.com>
2023-04-17 10:36:23 -0400

    remove forced cache control headers

--- next.config.js
+++ next.config.js
@@ -6,19 +6,6 @@ const nextConfig = {
   images: {
     domains: ["randomcontent.wpengine.com"],
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "cache-control",
-            value: "s-maxage=60, stale-while-revalidate, stale-if-error",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
