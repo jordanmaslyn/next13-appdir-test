8f2e96f7e51f189bf94409649d3cef53176061de
Jordan Maslyn <jordan.maslyn@wpengine.com>
2023-04-17 10:28:10 -0400

    Force cache control headers from next config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,19 @@ const nextConfig = {
   images: {
     domains: ["randomcontent.wpengine.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "cache-control",
+            value: "s-maxage=60, stale-while-revalidate, stale-if-error",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
